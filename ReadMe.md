e35c07f94f55fabf33a337dace3a233973bbcc4b
pynappo <lehtien.david@gmail.com>
2024-05-08 10:52:03 -0700

    update docs

--- ReadMe.md
+++ ReadMe.md
@@ -5,13 +5,37 @@ Team Name: “Insert Team Name”
 Raymond Lin: raymond.z.lin@sjsu.edu, Austin Trinh: xuanhuy.trinh@sjsu.edu, David Le: david.le05@sjsu.edu
 
 Problem:
-The course catalog only has search criteria with course name and professor, without ratings. RateMyProfessor has rating data, but only allows you to search up to one professor at a time, which is tedious and inconvenient for comparing professors. There is no method to search many professors simultaneously with sorted rating information. 
+The course catalog only has search criteria with course name and professor, without ratings. RateMyProfessor has rating data, but only allows you to search up to one professor at a time, which is tedious and inconvenient for comparing professors. There is no method to search many professors simultaneously with sorted rating information.
 
 Functionality:
-We will use a web scraper to gather data on RateMyProfessor and the SJSU catalog. With this data, we will create a Command Line Interface with commands that allow users to search for courses based on course name, rating, and professor. The user can then sort this data by rating or course, allowing people to compare professors more quickly. This application will cover the area of Web Scraping and Data Analysis. 
+We will use a web scraper to gather data on RateMyProfessor and the SJSU catalog. With this data, we will create a Command Line Interface with commands that allow users to search for courses based on course name, rating, and professor. The user can then sort this data by rating or course, allowing people to compare professors more quickly. This application will cover the area of Web Scraping and Data Analysis.
+
+Running:
+
+Install `tabulate` (for CLI only):
+```bash
+pip install tabulate
+# or:
+python -m venv venv
+source venv/bin/activate.sh
+```
+
+Then run either the CLI or GUI:
+
+```bash
+python cli.py
+python gui.py
+```
+
+GUI instructions:
+
+The searchbar at the bottom allows you to search by classes. Type in your query and press the searchbar. To reset the
+query, you can clear the searchbar and press enter or press the "Reset" button.
+
+You can control+click or shift+click to select multiple items. Once selected you can press the Compare button to open
+the professors on RateMyProfessor.
 
 Previous Works:
-
 
 https://github.com/Rodantny/Rate-My-Professor-Scraper-and-Search:
 
@@ -24,6 +48,6 @@ This project is effectively the same as what we’re describing, but it has a we
 High-Level Approach:
 We will send an HTTP GET request to https://www.sjsu.edu/classes/schedules/spring-2024.php to gather course catalog information, and build a scraper to get information from RateMyProfessors. Our command line interface will process this data to return sorted search results to users.
 
-Dependencies: 
+Dependencies:
 
 [Tabulate](https://pypi.org/project/tabulate/)
--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ import tkinter.ttk as ttk
 import difflib
 import webbrowser
 
+# This class is used to store teacher data
 class Teacher:
     def __init__(self, name: str, avgDifficulty: str, avgRating: float, numRatings: int, wouldTakeAgainPercent: float,
                  link: str, id: str):
@@ -20,6 +21,7 @@ class Teacher:
         self.link = link
         self.id = id
 
+# Load data from RateMyProfessors
 file1 = open("SJSUCourses.json")
 data = json.load(file1)
 teacher_course = {}
@@ -28,6 +30,7 @@ teacher_to_struct = {}
 teacher_by_id = {}
 course_names = set()
 
+# Load all the data into dictionaries and Teacher objects
 for teacher in data["data"]["search"]["teachers"]["edges"]:
     teacher_name = teacher["node"]["firstName"] + " " + teacher["node"]["lastName"]
     teacher_rmp_link = "https://www.ratemyprofessors.com/professor/"+str(base64.b64decode(teacher["node"]["id"])[8::].decode('utf-8'))
@@ -50,6 +53,7 @@ for teacher in data["data"]["search"]["teachers"]["edges"]:
         teacher_course[id].append(standard_course_name)
         course_teacher[standard_course_name].append(teacher_struct)
 
+# These are the titles of the headings
 teacher_headings = ["Name", "Difficulty/5", "Rating/5", "Would take again %?", "url"]
 class ProfessorGui:
     def __init__(self):
@@ -59,17 +63,29 @@ class ProfessorGui:
 
         self.window.title("SJSU Courses")
 
+        self._init_treeview()
+        self._arrange_gui()
+
+
+    def _init_treeview(self):
+        """
+        We use a treeview for a tabluar display of the data
+        """
         self.treeview = ttk.Treeview(columns=teacher_headings, show="headings")
 
+        # Add every teacher
         for item in teacher_by_id.values():
             self.treeview.insert('', tk.END, values=(item.name, item.avgDifficulty, item.avgRating,
-                                                 item.wouldTakeAgainPercent, item.link), iid = item.id)
+                                                     item.wouldTakeAgainPercent, item.link), iid = item.id)
+            # Add the courses they teach as sub-items
             for i, course in enumerate(teacher_course.get(item.id)):
                 self.treeview.insert(item.id, tk.END, values=("| COURSE:", course.upper(), "", "", ""), iid
                                      = f'course{i}{item.id}')
 
+        # This function sorts the treeview when a column is clicked
         def treeview_sort_column(tv: ttk.Treeview, col, reverse):
             l = [(tv.set(k, col), k) for k in tv.get_children()]
+            # Try sorting by number and fallback to string
             if len(l) > 0:
                 first = l[0][0]
                 print(first)
@@ -90,28 +106,36 @@ class ProfessorGui:
             # reverse sort next time
             tv.heading(col, text=col, command=lambda _col=col: \
                 treeview_sort_column(tv, _col, not reverse))
-        for col in teacher_headings:
 
+        # Add the headings and add the sort function to each
+        for col in teacher_headings:
             self.treeview.heading(col, text=col.title(), command=lambda c=col: treeview_sort_column(self.treeview, c,
                                                                                                     False))
-            # command=lambda c=col: sortby(self.tree, c, 0))
             # adjust the column's width to the header string
             self.treeview.column(col, width=tkFont.Font().measure(col.title()) + 100)
-        vsb = ttk.Scrollbar(orient="vertical",
-            command=self.treeview.yview)
-        hsb = ttk.Scrollbar(orient="horizontal",
-            command=self.treeview.xview)
-        self.treeview.configure(yscrollcommand=vsb.set,
-            xscrollcommand=hsb.set)
 
+
+    def _arrange_gui(self):
+        """
+        Arranges the GUI elements with pack
+        """
+        # Initialize scrollbars for the table
+        vsb = ttk.Scrollbar(orient="vertical",
+                            command=self.treeview.yview)
+        hsb = ttk.Scrollbar(orient="horizontal",
+                            command=self.treeview.xview)
+        self.treeview.configure(yscrollcommand=vsb.set,
+                                xscrollcommand=hsb.set)
+        # Put the Treeview and scrollbars at the top
         container = ttk.Frame()
         self.treeview.grid(column=0, row=0, sticky='nsew', in_=container)
         vsb.grid(column=1, row=0, sticky='ns', in_=container)
         hsb.grid(column=0, row=1, sticky='ew', in_=container)
         container.grid_columnconfigure(0, weight=1)
         container.grid_rowconfigure(0, weight=1)
-
         self.treeview.pack(side=tk.TOP)
+
+        # this bottom frame contains the searchbar and open in web browser button
         self.bottom_frame = tk.Frame(self.window)
         self.bottom_frame.pack(side=tk.BOTTOM)
         self.compare_button = tk.Button(self.bottom_frame, text="Compare Professors on RMP", command=self.compare_professors)
@@ -125,17 +149,27 @@ class ProfessorGui:
         self.window.bind("<Return>", func=lambda _: self.filter(self.entry.get()))
 
     def reset(self):
+        """
+        Resets the search bar
+        """
         self.entry.delete(0, tk.END)
         self.filter()
 
     def compare_professors(self):
+        """
+        Opens a new tab for each selected professor
+        """
         for selection in self.treeview.selection():
             if selection.startswith("course"):
                 continue
             webbrowser.open_new_tab(teacher_by_id[selection].link)
 
     def filter(self, course: str = ""):
+        """
+        Filters the treeview based on the course name (in the search bar)
+        """
         course = course.lower()
+        # Restore previously hidden items
         while len(self.hidden) > 0:
             id = self.hidden.pop()
             item = teacher_by_id[id]
@@ -143,6 +177,8 @@ class ProfessorGui:
         if (course == ""):
             # just restore stuff
             return
+
+        # Hide items that don't match the course
         for id in self.treeview.get_children():
             should_hide = True
             for taught_course in teacher_course.get(id):
